d6d80a07303cc5f1fe79b7ca80a48b58df5e9958
Antxon Eguiguren <antxonegi@gmail.com>
2020-02-10 18:47:33 +0100

    Commit Inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ModelComponent } from './model/model.component';
+import { TemplateComponent } from './template/template.component';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: TemplateComponent },
+  { path: 'model', component: ModelComponent },
+  { path: 'template', component: TemplateComponent },
+  { path: '**', component: ModelComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
